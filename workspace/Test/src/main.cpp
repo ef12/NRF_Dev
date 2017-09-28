bf0c25ce443752c12236ef403be01af886f542e2
Ehud <frankehud@gmail.com>
2017-09-28 10:26:53 +0200

    Add free style unit test project

---
+++ workspace/Test/src/main.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : Ehud
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "pwm.hpp"
+
+using namespace std;
+using namespace ns_pwm;
+
+int main() {
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	//const uint32_t pins[4] = {0x00000001, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF};
+	//simple_pwm pwm(pins,0x4001C000);
+
+	return 0;
+}
