6d90c588a5322dba2747f3056b9fab867b930067
Ehud <frankehud@gmail.com>
2017-10-25 11:01:35 +0200

    Making UART driver working with SDK hal

--- workspace/nrf52_freeRTOS_cpp/inc/uart.hpp
+++ workspace/nrf52_freeRTOS_cpp/inc/uart.hpp
@@ -11,23 +11,19 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
-
-#include "pca10040.h"
-#include "app_uart.h"
-#include "nrf_uart.h"
-#include "nrf_drv_uart.h"
-#include "app_error.h"
-
+// C++ freeRTOS headers
 #include "thread.hpp"
 #include "ticks.hpp"
 #include "utilities.hpp"
+// board header
+#include "boards.h"
+// Hal headers
+#include "nrf_uart.h"
+#include "nrf_drv_common.h"
 
 #define UART_TX_BUF_SIZE		256		/**< UART TX buffer size. */
 #define UART_RX_BUF_SIZE		256		/**< UART RX buffer size. */
-extern "C" {
-void uart_handle(app_uart_evt_t * p_event);
-void uart_init(void);
-}
+extern "C" void UART0_IRQHandler(void) __attribute((interrupt));
 
 using namespace cpp_freertos;
 using namespace std;
@@ -38,7 +34,7 @@ public:
 
 	virtual ~com_base() {
 	}
-	virtual bool open(void) = 0;
+	virtual bool open(uint8_t id, uint32_t baud_rate) = 0;
 	virtual bool close(void) = 0;
 	virtual void write(const uint8_t* buff, uint8_t length) = 0;
 	virtual uint8_t read(void) = 0;
@@ -74,30 +70,34 @@ private:
 	uint8_t byte = 0;
 
 	friend
-	void ::uart_handle(app_uart_evt_t * p_event);
+	void ::UART0_IRQHandler(void) __attribute((interrupt));
 };
 
 class com_serial: public com_base {
 public:
-	com_serial(const uint32_t rx_pin, const uint32_t tx_pin,
-			const nrf_uart_baudrate_t baud);
-	void init(void);
-	virtual bool open(void);
+	com_serial(const uint32_t rx_pin, const uint32_t tx_pin);
+	virtual bool open(uint8_t id, uint32_t baud_rate);
 	virtual bool close(void);
 	virtual void write(const uint8_t* buff, uint8_t length);
+	void write_byte(uint8_t byte) {
+		nrf_uart_txd_set(NRF_UART0, byte);
+	}
 	uint8_t read(void);
+	uint8_t read_byte(void) {
+		return (nrf_uart_rxd_get(NRF_UART0));
+	}
 	virtual bool is_empty(void);
 protected:
 
 private:
-	const app_uart_comm_params_t m_comm_params;
+	const uint32_t m_rx_pin, m_tx_pin;
 	uart_buff_t rx_buffer, tx_buffer;
 
 	static bool m_open;
 	static com_serial* interruptOwner;
 	bool m_tx_busy;
 	friend
-	void ::uart_handle(app_uart_evt_t * p_event);
+	void ::UART0_IRQHandler(void) __attribute((interrupt));
 };
 
 #endif /* INC_UART_HPP_ */
--- workspace/nrf52_freeRTOS_cpp/src/uart.cpp
+++ workspace/nrf52_freeRTOS_cpp/src/uart.cpp
@@ -11,67 +11,132 @@ bool com_serial::m_open;
 com_serial* com_serial::interruptOwner;
 uart_parser* uart_parser::interruptOwner;
 
-extern "C"
-void uart_handle(app_uart_evt_t * p_event) {
-	if (com_serial::interruptOwner != nullptr) {
-		uint8_t cr;
-		switch (p_event->evt_type) {
-		case APP_UART_COMMUNICATION_ERROR:
-			APP_ERROR_HANDLER(p_event->data.error_communication);
-			break;
-		case APP_UART_FIFO_ERROR:
-			APP_ERROR_HANDLER(p_event->data.error_code);
-			break;
-		case APP_UART_DATA_READY:
-			app_uart_get(&cr);
-			if (cr == '\r') {
+extern "C" void UART0_IRQHandler(void) {
+	nrf_drv_common_irq_disable(nrf_drv_get_IRQn((void *) NRF_UART0));
+	if (com_serial::interruptOwner) {
+		// RX
+		if (nrf_uart_event_check(NRF_UART0, NRF_UART_EVENT_RXDRDY)) {
+			nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_RXDRDY);
+//			app_uart_get(&cr);
+//			if (cr == '\r') {
+//				if (!(com_serial::interruptOwner->rx_buffer.empty())) {
+//					uart_parser::interruptOwner->packet_ready.GiveFromISR(NULL);
+//				}
+//			} else {
+//				com_serial::interruptOwner->rx_buffer.in(cr);
+//			}
+//			app_uart_put(cr);
+//			break;
+			uint8_t byte = com_serial::interruptOwner->read_byte();
+			if (byte == '\r') {
 				if (!(com_serial::interruptOwner->rx_buffer.empty())) {
 					uart_parser::interruptOwner->packet_ready.GiveFromISR(NULL);
 				}
 			} else {
-				com_serial::interruptOwner->rx_buffer.in(cr);
+				com_serial::interruptOwner->rx_buffer.in(byte);
 			}
-			app_uart_put(cr);
-			break;
-		case APP_UART_TX_EMPTY:
+			com_serial::interruptOwner->write_byte(byte);
+			nrf_uart_task_trigger(NRF_UART0, NRF_UART_TASK_STARTRX);
+		}
+		// TX
+		else if (nrf_uart_event_check(NRF_UART0, NRF_UART_EVENT_TXDRDY)) {
+			nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_TXDRDY);
 			if (!(com_serial::interruptOwner->tx_buffer.empty())) {
-				app_uart_put(com_serial::interruptOwner->tx_buffer.out());
+				com_serial::interruptOwner->m_tx_busy = true;
+				nrf_uart_task_trigger(NRF_UART0, NRF_UART_TASK_STARTTX);
+				com_serial::interruptOwner->write_byte(
+						com_serial::interruptOwner->tx_buffer.out());
 			} else {
 				com_serial::interruptOwner->m_tx_busy = false;
 			}
-
-			break;
-		default:
-			break;
+		}
+		// Check for errors
+		else if (nrf_uart_event_check(NRF_UART0, NRF_UART_EVENT_ERROR)) {
+			nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_ERROR);
+		} else if (nrf_uart_event_check(NRF_UART0, NRF_UART_EVENT_RXTO)) {
+			nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_RXTO);
+//		} else {
+//			nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_CTS);
+//			nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_NCTS);
 		}
 	}
+
+	nrf_drv_common_irq_enable(nrf_drv_get_IRQn((void *) NRF_UART0), 1);
 }
+//
+//void uart_handle(app_uart_evt_t * p_event) {
+//if (com_serial::interruptOwner != nullptr) {
+//	uint8_t cr;
+//	switch (p_event->evt_type) {
+//	case APP_UART_COMMUNICATION_ERROR:
+//		APP_ERROR_HANDLER(p_event->data.error_communication);
+//		break;
+//	case APP_UART_FIFO_ERROR:
+//		APP_ERROR_HANDLER(p_event->data.error_code);
+//		break;
+//	case APP_UART_DATA_READY:
+//		app_uart_get(&cr);
+//		if (cr == '\r') {
+//			if (!(com_serial::interruptOwner->rx_buffer.empty())) {
+//				uart_parser::interruptOwner->packet_ready.GiveFromISR(NULL);
+//			}
+//		} else {
+//			com_serial::interruptOwner->rx_buffer.in(cr);
+//		}
+//		app_uart_put(cr);
+//		break;
+//	case APP_UART_TX_EMPTY:
+//		if (!(com_serial::interruptOwner->tx_buffer.empty())) {
+//			app_uart_put(com_serial::interruptOwner->tx_buffer.out());
+//		} else {
+//			com_serial::interruptOwner->m_tx_busy = false;
+//		}
+//
+//		break;
+//	default:
+//		break;
+//	}
+//}
+//}
 
+com_serial::com_serial(const uint32_t rx_pin, const uint32_t tx_pin) :
+		m_rx_pin(rx_pin), m_tx_pin(tx_pin) {
+	//	apply_config
+	nrf_gpio_pin_set(m_rx_pin);
+	nrf_gpio_cfg_output(m_tx_pin);
+	nrf_gpio_cfg_input(m_rx_pin, NRF_GPIO_PIN_NOPULL);
+	nrf_uart_txrx_pins_set(NRF_UART0, m_tx_pin, m_rx_pin);
 
-com_serial::com_serial(const uint32_t rx_pin, const uint32_t tx_pin,
-		const nrf_uart_baudrate_t baud) :
-		m_comm_params( { rx_pin, tx_pin,
-		UART_PIN_DISCONNECTED, UART_PIN_DISCONNECTED,
-				APP_UART_FLOW_CONTROL_ENABLED, false, baud }) {
 	m_tx_busy = false;
 	m_rx_rdy = false;
 }
 
-bool com_serial::open(void) {
+bool com_serial::open(uint8_t id, uint32_t baud_rate) {
 	if (!m_open) {
-
 		m_tx_busy = false;
 		interruptOwner = this;
+		rx_buffer.clear();
+		tx_buffer.clear();
 
-		static ret_code_t err_code;
+		nrf_uart_baudrate_set(NRF_UART0, (nrf_uart_baudrate_t) baud_rate);
+		nrf_uart_configure(NRF_UART0, NRF_UART_PARITY_EXCLUDED,
+				NRF_UART_HWFC_DISABLED);
+		nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_RXDRDY);
+		nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_TXDRDY);
+		nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_ERROR);
+		nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_RXTO);
+		nrf_uart_int_enable(NRF_UART0,
+				NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_RXTO
+						| NRF_UART_INT_MASK_RXDRDY | NRF_UART_INT_MASK_ERROR);
+		nrf_drv_common_irq_enable(nrf_drv_get_IRQn((void *) NRF_UART0), 1);
 
-//		uart_task ;
-		APP_UART_FIFO_INIT(&m_comm_params, UART_RX_BUF_SIZE, UART_TX_BUF_SIZE,
-				uart_handle, APP_IRQ_PRIORITY_LOWEST, err_code);
+		nrf_uart_enable(NRF_UART0);
 
-		APP_ERROR_CHECK(err_code);
-
+		nrf_uart_task_trigger(NRF_UART0, NRF_UART_TASK_STARTTX);
+		nrf_uart_task_trigger(NRF_UART0, NRF_UART_TASK_STARTRX);
 		m_open = true;
+
+		write((uint8_t*) "*", 1);
 
 		return true;
 	}
@@ -81,6 +146,12 @@ bool com_serial::open(void) {
 bool com_serial::close(void) {
 	if (m_open) {
 		interruptOwner = nullptr;
+		nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_TXDRDY);
+		nrf_uart_event_clear(NRF_UART0, NRF_UART_EVENT_RXTO);
+		nrf_uart_int_disable(NRF_UART0,
+				NRF_UART_INT_MASK_TXDRDY | NRF_UART_INT_MASK_RXTO);
+		nrf_drv_common_irq_enable(nrf_drv_get_IRQn((void *) NRF_UART0), 1);
+		nrf_uart_disable(NRF_UART0);
 		m_open = false;
 
 		return true;
@@ -94,7 +165,7 @@ void com_serial::write(const uint8_t* buff, uint8_t length) {
 	}
 	if (!m_tx_busy) {
 		m_tx_busy = true;
-		app_uart_put(tx_buffer.out());
+		write_byte(tx_buffer.out());
 	}
 }
 
